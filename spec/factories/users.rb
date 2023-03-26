162e2796de610aa36bd15b407cd31418f7a5edf6
Mark Brewster <mark.brewster@flexmr.org>
2023-03-26 12:16:48 +0000

    Add devise helpers to spec suite

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@
 
 FactoryBot.define do
   factory :user do
+    sequence(:email) { |n| "test-#{n.to_s.rjust(3, "0")}@sample.com" }
+    password { "123456" }
+    first_name { 'Leslie' }
+    last_name { 'Smith'}
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'devise'
 require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
@@ -63,6 +65,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
 
 Shoulda::Matchers.configure do |config|
