f240503b156ba05ce1e8fa9e80af6320c418d945
Mark Brewster <mark.brewster@flexmr.org>
2023-03-26 12:36:35 +0000

    add first and last name to devise registrations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
+  before_action :permit_sign_up_params, if: :devise_controller?
+
+  private
+
+  def permit_sign_up_params
+    devise_parameter_sanitizer.permit(
+      :sign_up,
+      keys: [
+        :first_name,
+        :last_name,
+        :email,
+        :password,
+        :password_confirmation
+      ]
+    )
+  end
 end
