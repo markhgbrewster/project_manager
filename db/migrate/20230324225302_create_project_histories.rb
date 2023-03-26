0a1e242eb6fef5f188b2306000617b4ced7686a4
Mark Brewster <mark.brewster@flexmr.org>
2023-03-26 13:10:54 +0000

    add migration to create additional models

---
+++ db/migrate/20230324225302_create_project_histories.rb
@@ -0,0 +1,11 @@
+class CreateProjectHistories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :project_histories do |t|
+      t.text :description
+      t.references :user, foreign_key: true
+      t.references :project, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_24_155004) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_24_225302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "message"
+    t.bigint "user_id", null: false
+    t.string "commentable_type", null: false
+    t.bigint "commentable_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "project_histories", force: :cascade do |t|
+    t.text "description"
+    t.bigint "user_id"
+    t.bigint "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_project_histories_on_project_id"
+    t.index ["user_id"], name: "index_project_histories_on_user_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -40,5 +59,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_24_155004) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "users"
+  add_foreign_key "project_histories", "projects"
+  add_foreign_key "project_histories", "users"
   add_foreign_key "projects", "users"
 end
